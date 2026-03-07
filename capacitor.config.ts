67713f3291960525ddd43ef003454cebf30e1be7
Aaron Mimoun <armoun26@gmail.com>
2026-03-07 21:18:34 +0200

    documents 4

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.myevent.app',
+  appId: 'com.aaronmimoun.myevent',
   appName: 'MyEvent',
   webDir: 'dist'
 };
